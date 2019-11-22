52aac65e6d9d7011bf30945abcb993a712c7d3af
jonasns <jonas.sondergaard@ki.se>
2019-11-22 12:54:14 +0100

    script to build reference genome index files for Bowtie2

---
+++ 161123_Bowtie2_GRCh38.p7_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#script to build reference genome index files for Bowtie2
+#made by Jonas N. Søndergaard, 161123
+
+#UPPMAX commands (Uppsala Multidisciplinary Center for Advanced Computational Science)
+#SBATCH -A uppmax_proj_number
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 2:00:00
+
+#load packages. bioinfo-tools is loaded on uppmax in order to load all other packages used.
+module load bioinfo-tools
+module load bowtie2/2.2.9
+
+#use Bowtie2 to build reference genome
+bowtie2-build \
+	/proj/ref_genomes/GRCh38.p7.genome.fa \
+	/proj/ref_genomes/GRCh38.p7.genome \
+
